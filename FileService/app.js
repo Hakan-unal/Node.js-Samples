b68fb76e5f72a597b4fc02ba453505e0d277d51c
Hakan-unal <hakanunal_1996@hotmail.com>
2020-03-08 00:52:24 +0300

    updated

--- FileService/app.js
+++ FileService/app.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const expressApp = express();
-const eventEmitter = require('events');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 expressApp.set('view engine', 'pug');
 expressApp.set('views', 'views');
 
-expressApp.use('/form', function (req, res) {
+expressApp.use(bodyParser.urlencoded({ extended: false }));
+
+router.get('/', function (req, res) {
     res.render('form');
     res.end();
 });
 
-expressApp.use('/', function (req, res) {
-    res.send('Merhaba devam etmek için URL adresinizin sonuna form ekleyin<br /> örnek:<br />http://localhost:3000/form ');
-    res.end();
+router.post('/', function (req, res) {
+    console.log("Merhaba");
 });
-
-
-
-
-
 
 expressApp.listen(3000, () => {
     console.log('Server listening port 3000');
 });
-
-
