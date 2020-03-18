70ed8ee6837faecf9db7181c94210cf03b807b48
Hakan-unal <hakanunal_1996@hotmail.com>
2020-03-18 23:03:27 +0300

    version1

--- FileService/app.js
+++ FileService/app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const expressApp = express();
-const router = express.Router();
 const bodyParser = require('body-parser');
+const fs = require("fs");
 
 expressApp.set('view engine', 'pug');
 expressApp.set('views', 'views');
 
 expressApp.use(bodyParser.urlencoded({ extended: false }));
 
-router.get('/', function (req, res) {
+
+expressApp.get('/', (req, res) => {
     res.render('form');
-    res.end();
 });
 
-router.post('/', function (req, res) {
-    console.log("Merhaba");
+expressApp.post('/', (req, res) => {
+    var formText = req.body.text;
+    fs.writeFile("form.txt", formText, (error) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("File created");
+        }
+    });
+    res.redirect("/");
 });
 
 expressApp.listen(3000, () => {
